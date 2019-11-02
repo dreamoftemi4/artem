912c1ed4fa5a451cf2c6189bcd8ef42351c06e54
Студент МПТ <student@dc.mpt.ru>
2019-11-02 16:24:04 +0300

    first project

---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double i = 5;
+            double j = 0;
+
+            while (i > -1000)
+            {
+                i -= j;
+                j--;
+            }
+            Console.WriteLine("Summa = " + i);
+
+            Console.WriteLine("Ответ = {0}", i / j);
+            Console.ReadKey();
+        }
+    }
+}
